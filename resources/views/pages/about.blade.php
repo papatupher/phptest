a5d4a8f39a42225a42238da813d1c6b143d4c951
vagrant <vagrant@homestead>
2020-04-07 01:56:48 +0000

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>ABOUT TO HAH</h1>
+    <p>ITO AY TUNGKOL SA ABOUT</p>
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>{{$post->title}}</h1>
+    <div>
+        {{$post->body}}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}}</small>
+@endsection
